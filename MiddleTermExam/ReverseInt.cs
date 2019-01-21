c71290a05b8d639aeccf78c446b9bb3fc8a0ae2a
nitan2001 <nitan2001@163.com>
2019-01-21 23:51:01 +0800

    deep study the way of pop and push

---
+++ MiddleTermExam/ReverseInt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleTermExam
+{
+    class ReverseInt
+    {
+        public int Reverse(int x)
+        {
+            int ret = 0;
+
+            while (x != 0)
+            {
+                int nextDigit = x % 10;
+                x = x / 10;
+
+                // c# way to check for int overflow
+                try
+                {
+                    ret = checked((ret * 10) + nextDigit);
+                }
+                catch (System.OverflowException)
+                {
+                    // overflow
+                    return 0;
+                }
+            }
+
+            return ret;
+        }
+    }
+}
