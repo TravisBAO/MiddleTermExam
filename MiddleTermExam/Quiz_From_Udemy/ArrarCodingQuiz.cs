649457b567109e31aff761d00450e26c2c5cffa8
Bao Yi <yi.bao.ext@siemens.com>
2019-01-30 16:43:48 +0800

    1

--- MiddleTermExam/Quiz_From_Udemy/ArrarCodingQuiz.cs
+++ MiddleTermExam/Quiz_From_Udemy/ArrarCodingQuiz.cs
@@ -12,8 +12,17 @@ namespace MiddleTermExam.Quiz_From_Udemy
         
         public int AdditionIntegers()
         {
-            Console.WriteLine("Input the value:");
-            Console.Read();
+
+            int[] ValidIntegers = new int[10];
+            bool ValidInput = true; 
+                
+            while (ValidInput)
+            {
+                Console.WriteLine("Input the integer value between 0 and 10 (input -1 to exit):");
+                string inputValue = Console.ReadLine();
+            }
+
+
             return 1;
         }
     }
